4caf9be6592e6e0cc10e57db9ad728baabb88025
JuhisVain <Juho.Vainio@protonmail.ch>
2021-10-23 10:31:37 +0300

    Use define-ic for signal & printer

--- gates.lisp
+++ gates.lisp
@@ -11,36 +11,22 @@
   (defparameter *signal-2* (make-signal))
   (defparameter *printer* (make-printer))
   (defparameter *and* (make-and))
-  (wire ((csignal-out *signal-1*) 1 (and-a *and*)))
-  (wire ((csignal-out *signal-2*) 1 (and-b *and*)))
-  (wire ((and-out *and*) 1 (printer-in *printer*))))
+  (wire ((signal-out *signal-1*) 1 (and-a *and*)))
+  (wire ((signal-out *signal-2*) 1 (and-b *and*)))
+  (wire ((and-out *and*) 1 (printer-in *printer*)))
+  (set-output (signal-out *signal-1*) 1 0)
+  (set-output (signal-out *signal-2*) 1 0)
+  (roll-events))
 
-(defstruct (csignal (:include ic) (:constructor make-signal))
-  (out (make-output-pin)))
-(store-component-pin-list 'csignal
-			  '((out csignal-out)))
+(define-ic (signal-source :alias signal)
+  :pins
+  ((out :output)))
 
-(defstruct (printer (:include ic) (:constructor raw-make-printer))
-  (in (make-drive-pin :name 'in)))
-(store-component-pin-list 'printer
-			  '((in printer-in)))
-(setf (gethash 'printer *event-processor-table*)
-      #'(lambda (chip source time)
-	  (macrolet ((output (&rest pin-values)
-		       `(progn ,@(loop for (pin value) on pin-values by #'cddr
-				       collect `(set-output ,pin ,value time))))
-		     (floating (&rest pins)
-		       `(progn ,@(loop for pin in pins
-				       collect `(cut-output ,pin time)))))
-	    (with-pins-and-registers printer chip
-	      ;; Note that this will only print on change of input.
-	      ;; That is the rise or fall of the input signal.
-	      (format t "Receiving ~a!~%" (pin-input in))))))
-(defun make-printer ()
-  (let ((new-printer (raw-make-printer)))
-    (setf (drive-pin-chip (printer-in new-printer)) new-printer
-	  (ic-event-processor new-printer) (gethash 'printer *event-processor-table*))
-    new-printer))
+(define-ic printer
+  :pins
+  ((in :drive))
+  :event-processor
+  ((in (format t "Receiving ~a!~%" (pin-input in)))))
 
 (define-ic (and-gate :alias and)
   :pins
