3ea8665983c3e2bdf8d0df1d71ecea6a7d8acd44
JuhisVain <Juho.Vainio@protonmail.ch>
2021-11-04 12:46:39 +0200

    Fix uinteger-bits converting 0 drops a bit

--- circuit.lisp
+++ circuit.lisp
@@ -425,18 +425,21 @@ wire
   (equal a b))
 
 (defun uinteger-bits (integer &optional (minimum-length 1))
-  (let ((bits nil))
-    (loop while (plusp integer)
-	  for i from 1
-	  do (multiple-value-bind (quotient remainder)
-		 (floor integer 2)
-	       (setf integer quotient)
-	       (push remainder bits))
-	  finally (if (< i minimum-length)
-		      (setf bits (append (make-list (- minimum-length i)
-						    :initial-element 0)
-					 bits))))
-    (reverse (bits bits))))
+  (cond ((zerop integer)
+	 (make-array minimum-length :element-type 'bit :initial-element 0))
+	(t
+	 (let ((bits nil))
+	   (loop while (plusp integer)
+		 for i from 1
+		 do (multiple-value-bind (quotient remainder)
+			(floor integer 2)
+		      (setf integer quotient)
+		      (push remainder bits))
+		 finally (if (< i minimum-length)
+			     (setf bits (append (make-list (- minimum-length i)
+							   :initial-element 0)
+						bits))))
+	   (reverse (bits bits))))))
 
 (defun signed-bit-integer (bit-array)
   (cond ((bit-plusp bit-array)
